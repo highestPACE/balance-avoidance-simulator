43127ae9f288533b5b3c7777676943eec5cbd012
highestPACE <pschweizer0711@gmail.com>
2026-03-19 12:16:55 +0100

    Fixed error in BalancingController-function. Balance is now restored, however robot drives away because linearVelocity was not taken into account

--- balance-avoidance-simulator/src/simulation/BalancingController.java
+++ balance-avoidance-simulator/src/simulation/BalancingController.java
@@ -4,15 +4,17 @@ import physics.Physics;
 
 public class BalancingController {
 
-    public static double controlWheelAngularAcceleration(double dt, double length, double angle) {
+    public static double controlWheelAngularAcceleration(double length, double angle, double angularVelocity,
+	    double dt) {
 	if (angle == 0) {
-	    return 0;
+	    return 0; // Result of Formula in else
 	} else if (angle == Math.PI / 2) {
 	    return Double.POSITIVE_INFINITY;
 	} else if (angle == (3 * Math.PI) / 2) {
 	    return Double.NEGATIVE_INFINITY;
 	} else {
-	    return (Physics.G * Math.sin(angle) + (angle * length) / dt) / Math.cos(angle);
+	    return (Physics.G * Math.sin(angle) + length * ((angle / (dt * dt)) + (angularVelocity / dt)))
+		    / Math.cos(angle);
 	}
     }
 }
