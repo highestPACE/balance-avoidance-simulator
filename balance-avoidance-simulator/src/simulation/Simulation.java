2d8878144461f4e9f7d65187fd452b53a09f5350
highestPACE <pschweizer0711@gmail.com>
2026-03-19 15:43:16 +0100

    In Simulation.step(): Added velocity-term to WeightedController and heuristically tuned the parameters

--- balance-avoidance-simulator/src/simulation/Simulation.java
+++ balance-avoidance-simulator/src/simulation/Simulation.java
@@ -16,14 +16,13 @@ public class Simulation {
 	if (!(0 <= translatedAngle && translatedAngle <= Math.PI)) {
 	    translatedAngle -= 2 * Math.PI; // Translate angle to be in [-pi, pi] where 0 is the desired position on top
 	}
-	if (Math.abs(translatedAngle) <= (Math.PI / 2)) {
-	    translatedAngle = -translatedAngle;
-	}
-	ControlTerm angleTerm = new ControlTerm("angle", 100, translatedAngle);
-	ControlTerm positionTerm = new ControlTerm("position", 100, getRobotXPosition());
+	ControlTerm angleTerm = new ControlTerm("angle", 100, -translatedAngle);
+	ControlTerm velocityTerm = new ControlTerm("velocity", 1, -getRobotAngularVelocity());
+	ControlTerm positionTerm = new ControlTerm("position", 100, -getRobotXPosition());
 	WeightedController controller = new WeightedController();
 	controller.addTerm(angleTerm);
-	// controller.addTerm(positionTerm);
+	controller.addTerm(velocityTerm);
+	controller.addTerm(positionTerm);
 
 	double wheelAngAcc = controller.computeOutput();
 	robot.getWheel().setAngularAcceleration(wheelAngAcc);
