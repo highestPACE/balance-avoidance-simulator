364df5e9d3e8c433cb9aa837d0457176c3fc7c81
highestPACE <pschweizer0711@gmail.com>
2026-03-19 13:18:33 +0100

    Fixed PIDBalancingController

--- balance-avoidance-simulator/src/simulation/PIDBalancingController.java
+++ balance-avoidance-simulator/src/simulation/PIDBalancingController.java
@@ -3,7 +3,6 @@ package simulation;
 public class PIDBalancingController {
 
     public double control(double pFactor, double iFactor, double dFactor, double angle, double angularVelocity) {
-
-	return pFactor * angle + dFactor * angularVelocity;
+	return - pFactor * angle - dFactor * angularVelocity; // Does not use I-Term
     }
 }
