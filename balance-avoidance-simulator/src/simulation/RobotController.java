1b541f1f6d2528d43a86c7449756f7dbf18179ed
highestPACE <pschweizer0711@gmail.com>
2026-03-21 14:58:03 +0100

    Implemented RobotController-class to capsule controller-code

---
+++ balance-avoidance-simulator/src/simulation/RobotController.java
@@ -0,0 +1,25 @@
+package simulation;
+
+import physics.Physics;
+
+public class RobotController {
+
+    private double tMin;
+
+    public RobotController(double tMin) {
+	setTMin(tMin);
+    }
+
+    public double controlWheelAngularAcceleration(double radius, double length, double angle, double angularVelocity) {
+	return (Physics.G * Math.sin(angle) + ((2 * length) / (tMin * tMin)) * (angle + tMin * angularVelocity))
+		/ (Math.cos(angle) * radius);
+    }
+
+    public double getTMin() {
+	return tMin;
+    }
+
+    public void setTMin(double tMin) {
+	this.tMin = tMin;
+    }
+}
