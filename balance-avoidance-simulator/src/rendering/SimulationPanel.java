d192928bca5bc0c2c6d3dd82fbbbb5596e768b59
highestPACE <pschweizer0711@gmail.com>
2026-03-24 10:53:41 +0100

    Updated Main and SimulationPanel to the changes for the calculation of robot-/wheel-state and their visibility

--- balance-avoidance-simulator/src/rendering/SimulationPanel.java
+++ balance-avoidance-simulator/src/rendering/SimulationPanel.java
@@ -18,6 +18,8 @@ public class SimulationPanel extends JPanel {
     protected void paintComponent(Graphics g) {
 	super.paintComponent(g);
 
+	simulation.Robot robot = simulation.getRobot();
+
 	Graphics g2 = (Graphics2D) g;
 
 	int width = getWidth();
@@ -25,13 +27,13 @@ public class SimulationPanel extends JPanel {
 
 	double groundY = height / 2;
 	double scale = 1000;
-	double wheelRadius = simulation.getWheelRadius();
+	double wheelRadius = robot.getWheelRadius();
 
-	double wheelX = simulation.getRobotXPosition() * scale + (width / 2);
+	double wheelX = robot.getXPosition() * scale + (width / 2);
 	double wheelY = groundY - wheelRadius * scale;
 
-	double topX = wheelX + Math.sin(simulation.getRobotAngle()) * simulation.getRobotLength() * scale;
-	double topY = wheelY - Math.cos(simulation.getRobotAngle()) * simulation.getRobotLength() * scale;
+	double topX = wheelX + Math.sin(robot.getAngle()) * robot.getLength() * scale;
+	double topY = wheelY - Math.cos(robot.getAngle()) * robot.getLength() * scale;
 
 	// ground
 	g2.drawLine(0, (int) groundY, width, (int) groundY);
@@ -44,15 +46,14 @@ public class SimulationPanel extends JPanel {
 	g2.drawLine((int) wheelX, (int) wheelY, (int) topX, (int) topY);
 
 	// Text
-	g2.drawString(String.format("Robot length: %.3f", simulation.getRobotLength()), 10, 10);
-	g2.drawString(String.format("Wheel radius: %.3f", simulation.getWheelRadius()), 10, 20);
-	g2.drawString(String.format("Robot x-position: %.3f", simulation.getRobotXPosition()), 10, 30);
-	g2.drawString(String.format("Robot angle: %.3f", simulation.getRobotAngle()), 10, 40);
-	g2.drawString(String.format("Robot angular velocity: %.3f", simulation.getRobotAngularVelocity()), 10, 50);
-	g2.drawString(String.format("Robot angular acceleration: %.3f", simulation.getRobotAngularVelocity()), 10, 60);
-	g2.drawString(String.format("Wheel linear velocity: %.3f", simulation.getWheelLinearVelocity()), 10, 70);
-	g2.drawString(String.format("Wheel angular velocity: %.3f", simulation.getWheelAngularVelocity()), 10, 80);
-	g2.drawString(String.format("Wheel angular acceleration: %.3f", simulation.getWheelAngularAcceleration()), 10,
-		90);
+	g2.drawString(String.format("Robot length: %.3f", robot.getLength()), 10, 10);
+	g2.drawString(String.format("Wheel radius: %.3f", robot.getWheelRadius()), 10, 20);
+	g2.drawString(String.format("Robot x-position: %.3f", robot.getXPosition()), 10, 30);
+	g2.drawString(String.format("Robot angle: %.3f", robot.getAngle()), 10, 40);
+	g2.drawString(String.format("Robot angular velocity: %.3f", robot.getAngularVelocity()), 10, 50);
+	g2.drawString(String.format("Robot angular acceleration: %.3f", robot.getAngularVelocity()), 10, 60);
+	g2.drawString(String.format("Wheel linear velocity: %.3f", robot.getWheelLinearVelocity()), 10, 70);
+	g2.drawString(String.format("Wheel angular velocity: %.3f", robot.getWheelAngularVelocity()), 10, 80);
+	g2.drawString(String.format("Wheel angular acceleration: %.3f", robot.getWheelAngularAcceleration()), 10, 90);
     }
 }
