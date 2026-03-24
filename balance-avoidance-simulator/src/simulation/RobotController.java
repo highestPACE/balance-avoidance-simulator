9d8d0a7d7baac2762d9f4d7e0924c93d794bf4a2
highestPACE <pschweizer0711@gmail.com>
2026-03-24 09:44:53 +0100

    RobotController can now achieve arbitrary angles with RobotController.achieveAngle()

--- balance-avoidance-simulator/src/simulation/RobotController.java
+++ balance-avoidance-simulator/src/simulation/RobotController.java
@@ -11,11 +11,13 @@ public class RobotController {
     }
 
     public double controlWheelAngularAcceleration(double radius, double length, double angle, double angularVelocity) {
-	return achieveBalance(radius, length, angle, angularVelocity);
+	return achieveAngle(0, radius, length, angle, angularVelocity);
     }
 
-    private double achieveBalance(double radius, double length, double angle, double angularVelocity) {
-	return (Physics.G * Math.sin(angle) + ((2 * length) / (tMin * tMin)) * (angle + tMin * angularVelocity))
+    private double achieveAngle(double desiredAngle, double radius, double length, double angle,
+	    double angularVelocity) {
+	return (Physics.G * Math.sin(angle)
+		- ((2 * length) / (tMin * tMin)) * (desiredAngle - angle - tMin * angularVelocity))
 		/ (Math.cos(angle) * radius);
     }
 
