d6f41dfd83d80142dd6fcbad376f9554791fb8f7
highestPACE <pschweizer0711@gmail.com>
2026-03-24 10:58:26 +0100

    RobotController-methods now take the whole Robot-intance as parameter

--- balance-avoidance-simulator/src/controller/RobotController.java
+++ balance-avoidance-simulator/src/controller/RobotController.java
@@ -1,6 +1,7 @@
-package simulation;
+package controller;
 
 import physics.Physics;
+import simulation.Robot;
 
 public class RobotController {
 
@@ -10,15 +11,21 @@ public class RobotController {
 	setTMin(tMin);
     }
 
-    public double controlWheelAngularAcceleration(double radius, double length, double angle, double angularVelocity) {
-	return achieveAngle(0, radius, length, angle, angularVelocity);
+    public double controlWheelAngularAcceleration(Robot robot) {
+	return achieveAngle(0, robot);
     }
 
-    private double achieveAngle(double desiredAngle, double radius, double length, double angle,
-	    double angularVelocity) {
-	return (Physics.G * Math.sin(angle)
-		- ((2 * length) / (tMin * tMin)) * (desiredAngle - angle - tMin * angularVelocity))
-		/ (Math.cos(angle) * radius);
+    private double achieveAngle(double desiredAngle, Robot robot) {
+	return (Physics.G * Math.sin(robot.getAngle()) - ((2 * robot.getLength()) / (tMin * tMin))
+		* (desiredAngle - robot.getAngle() - tMin * robot.getAngularVelocity()))
+		/ (Math.cos(robot.getAngle()) * robot.getWheelRadius());
+    }
+
+    private double achievePosition(double desiredPosition, Robot robot) {
+	double linearAcceleration = (2 / (tMin * tMin))
+		* (desiredPosition - robot.getXPosition() - tMin * robot.getWheelLinearVelocity());
+	double desiredAngle = Math.atan(linearAcceleration / Physics.G);
+	return achieveAngle(desiredAngle, robot);
     }
 
     public double getTMin() {
--- balance-avoidance-simulator/src/simulation/Simulation.java
+++ balance-avoidance-simulator/src/simulation/Simulation.java
@@ -15,11 +15,11 @@ public class Simulation {
 	double tMin = 0.1; // controller wants to ensure that goal is not met before tMin seconds went by
 	RobotController controller = new RobotController(tMin);
 
-	double wheelAngAcc = controller.controlWheelAngularAcceleration(robot.getWheelRadius(), robot.getLength(),
-		robot.getAngle(), robot.getAngularVelocity());
+	double wheelAngAcc = controller.controlWheelAngularAcceleration(getRobot());
 	robot.setWheelAngularAcceleration(wheelAngAcc);
 
-	double wheelAngVel = Physics.valueChange(robot.getWheelAngularVelocity(), robot.getWheelAngularAcceleration(), dt);
+	double wheelAngVel = Physics.valueChange(robot.getWheelAngularVelocity(), robot.getWheelAngularAcceleration(),
+		dt);
 	robot.setWheelAngularVelocity(wheelAngVel);
 
 	double robAngVel = Physics.valueChange(robot.getAngularVelocity(), robot.getAngularAcceleration(), dt);
@@ -31,7 +31,7 @@ public class Simulation {
 	double robXPosition = Physics.valueChange(robot.getXPosition(), robot.getWheelLinearVelocity(), dt);
 	robot.setXPosition(robXPosition);
     }
-    
+
     public Robot getRobot() {
 	return robot;
     }
